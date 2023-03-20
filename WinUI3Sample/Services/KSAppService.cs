88d7fdd678ce0728aa893ae025eb59da655a0a9a
KS.STUDIO <l_z_xin@msn.cn>
2023-03-21 01:29:48 +0800

    2023年3月21日01点29分

--- WinUI3Sample/Services/KSAppService.cs
+++ WinUI3Sample/Services/KSAppService.cs
@@ -227,9 +227,7 @@ public class KSAppService
     /// <param name="iconName"></param>
     public void LoadIcon(Window window,string iconName)
     {
-        var hwnd = GetHwnd(window);
-        var hIcon = PInvoke.User32.LoadImage(IntPtr.Zero, iconName, PInvoke.User32.ImageType.IMAGE_ICON, 16, 16, PInvoke.User32.LoadImageFlags.LR_LOADFROMFILE);
-        PInvoke.User32.SendMessage(hwnd, PInvoke.User32.WindowMessage.WM_SETICON, (IntPtr)0, hIcon);
+        GetAppWindowForCurrentWindow(window).SetIcon(Path.Combine(AppContext.BaseDirectory, iconName));
     }
     /// <summary>
     /// AppWindow
